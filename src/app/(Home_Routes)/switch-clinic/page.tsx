e63f21579c00a12ebc8c191c3e92cb73e7be83b3
kritesh2nd <kritesh2nd@gmail.com>
2025-08-13 10:45:17 +0545

    feat: add switch-clinic page +3 sections

--- src/app/(Home_Routes)/switch-clinic/page.tsx
+++ src/app/(Home_Routes)/switch-clinic/page.tsx
@@ -4,8 +4,16 @@ import Image from "next/image";
 import Link from "next/link";
 import React, { ChangeEvent, useState } from "react";
 import { FiArrowRight } from "react-icons/fi";
+import { FaCheck } from "react-icons/fa";
 
 const SwitchClinic = () => {
+  const meetHerbal = [
+    { id: 1, text: "Certified Practitioners" },
+    { id: 2, text: "Personalized Care" },
+    { id: 3, text: "Evidence-Based Approach" },
+    { id: 4, text: "Ongoing Support" },
+  ];
+
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -190,12 +198,69 @@ const SwitchClinic = () => {
         </div>
       </section>
       {/* section 3 */}
-      <section className="">
-        <div className="globalContainer">Section 3</div>
+      <section className="py-40 border border-stone-300">
+        <div className="globalContainer">What Our Members Says</div>
       </section>
       {/* section 4 */}
       <section className="">
-        <div className="globalContainer">Section 4</div>
+        <div className="globalContainer py-[70px]">
+          <div className="gooper text-center text-farm-green md:text-[39px] sm:text-[31px] text-[25px] pb-[64px]">
+            Meed Herbal Opinion
+          </div>
+          <div className="flex flex-col sm:flex-row">
+            <div className="flex flex-1 justify-center sm:pb-0 pb-8">
+              <div className="lg:h-[650px] md:h-[520px] sm:h-[420px] h-[320px] lg:w-[650px] md:w-[520px] sm:w-[420px] w-[320px] relative">
+                <Image
+                  src="/img/switch-clinic/meetup.png"
+                  fill
+                  alt="yoga girl"
+                />
+              </div>
+            </div>
+            <div className="flex justify-end items-center flex-1">
+              <div className="sm:pl-[105px]">
+                <p className="text-primary-dgray text-[18px] sm:text-[20px]">
+                  Founded by an Australian citizen with a mission to make
+                  natural health support accessible to everyone, Herbal Opinion
+                  is committed to compassionate care through personalized
+                  solutions.
+                </p>
+                <br />
+                <p className="text-primary-dgray text-[18px] sm:text-[20px]">
+                  We believe in the power of nature to support wellness and
+                  provide relief. Our team of healthcare professionals carefully
+                  evaluates each person's needs to recommend the most
+                  appropriate natural solutions.
+                </p>
+                <br />
+                <div className="grid grid-cols-2 sm:pb-9 pb-5">
+                  {meetHerbal.map((item) => (
+                    <div
+                      className="flex gap-2 text-primary-dgray text-[18px] sm:text-[20px]  pb-3"
+                      key={item.id}
+                    >
+                      <div className="flex justify-center items-center text-white text-[12px] h-4 w-4 bg-farm-green rounded-full mt-[2px]">
+                        <FaCheck />
+                      </div>
+                      <p>{item.text}</p>
+                    </div>
+                  ))}
+                </div>
+                <div className="flex ">
+                  <Link
+                    href="/"
+                    className="flex items-center gap-3 bg-lemon text-farm-green px-7 py-[5px] rounded-full sm:text-[25px] text-[20px] transition-all duration-300 hover:shadow-lg hover:scale-105 cursor-pointer"
+                  >
+                    <div className="flex items-center gap-3 pt-[3px] md:pt-[6px] ">
+                      Start Your Journey
+                      <FiArrowRight className="w-5 h-5 group-hover:translate-x-1 transition-transform duration-300" />
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
