575c4654970d773d1e66ca5cf184afa55e018086
kritesh2nd <kritesh2nd@gmail.com>
2025-08-14 15:44:00 +0545

    fix: filename in Steps component

--- src/components/Steps.tsx
+++ src/components/Steps.tsx
@@ -1,10 +1,10 @@
 "use client";
 import React from "react";
-import { JourneyStep } from "../types";
+import { JourneyStepType } from "../types";
 import Image from "next/image";
 import { FaCheckCircle } from "react-icons/fa";
 
-const Steps = ({ item }: { item: JourneyStep }) => {
+const Steps = ({ item }: { item: JourneyStepType }) => {
   return (
     <div className="">
       <div className="flex  gap-5">
